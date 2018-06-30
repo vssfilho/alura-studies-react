0431f859810048dec1e0959b9b250266e1f07a4b
Valter Filho <valter.filho@ymail.com>
2018-06-30 15:40:48 -0300

    Criação de componentes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
+import InputComponet from './componets/InputComponet';
 
 class App extends Component {
 
   constructor() {
     super();
-    this.state = {lista : []}; 
+    this.state = {
+      lista : [],
+      nome: '',
+      email: '',
+      senha: ''
+    }; 
+    this.cadastrarAutor = this.cadastrarAutor.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
   }
 
   componentDidMount() {
@@ -17,6 +27,35 @@ class App extends Component {
       success: function(data) {
         this.setState({lista:data});
       }.bind(this) 
+    });
+  }
+
+  setNome(event) {
+    this.setState({nome: event.target.value});
+  }
+
+  setEmail(event) {
+    this.setState({email: event.target.value});
+  }
+
+  setSenha(event) {
+    this.setState({senha: event.target.value});
+  }
+
+  cadastrarAutor(event) {
+    event.preventDefault();
+    $.ajax({
+      url:"http://cdc-react.herokuapp.com/api/autores",
+      contentType: 'application/json',
+      dataType:'json',
+      type:'post',
+      data: JSON.stringify({nome:this.state.nome, email: this.state.email, senha:this.state.senha}),
+      success: function(data){
+        this.setState({lista: data});
+      }.bind(this),
+      error: function(data){
+        console.log("erro");
+      }
     });
   }
 
@@ -47,19 +86,10 @@ class App extends Component {
           <br />
           <div className="content" id="content">
             <div className="pure-form pure-form-aligned">
-              <form className="pure-form pure-form-aligned">
-                <div className="pure-control-group">
-                  <label htmlFor="nome">Nome</label> 
-                  <input id="nome" type="text" name="nome" value=""  />                  
-                </div>
-                <div className="pure-control-group">
-                  <label htmlFor="email">Email</label> 
-                  <input id="email" type="email" name="email" value=""  />                  
-                </div>
-                <div className="pure-control-group">
-                  <label htmlFor="senha">Senha</label> 
-                  <input id="senha" type="password" name="senha"  />                                      
-                </div>
+              <form className="pure-form pure-form-aligned" onSubmit={this.cadastrarAutor} >
+                <InputComponet id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome" />
+                <InputComponet id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email" />
+                <InputComponet id="senha" type="password" name="nome" value={this.state.senha} onChange={this.setSenha} label="Senha" />
                 <div className="pure-control-group">                                  
                   <label></label> 
                   <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
